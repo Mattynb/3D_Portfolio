fef7cd8f5256f3ad42fae3b78ceaa2654e7c09a8
matheus <matheus.berbet001@umb.edu>
2024-01-02 19:54:55 -0500

    Update About and Works components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -42,7 +42,7 @@ const About = () => {
         Something about myself
       </motion.p>
 
-      <div className='mt-20 flex flex-wrap gap-10'>
+      <div className='mt-20 flex flex-wrap gap-10 justify-center'>
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))} 
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -7,7 +7,51 @@ import {projects } from '../constants';
 import {fadeIn, textVariant} from '../utils/motion';
 
 const ProjectCard = ( {index, name, description, tags, image, source_code_link}) => {
- 
+ return (
+  <motion.div variants={fadeIn('Up', 'spring', index*0.5, 0.75)}>
+    <Tilt
+      className='bg-tertiary p-5 rounded-2x1 sm:w-[360px] w-full' 
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450
+      }}
+    >
+      <div className='relative w-full h-[230px]'>
+        <img 
+          src={image}
+          alt={name}
+          className='w-full h-full object-cover rounded-2x1'
+        />
+        <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+          <div 
+            onClick={() => window.open(source_code_link, '_blank')}
+            className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'  
+          >
+            <img src={github} alt='github' className='w-1/2 h-1/2 object-contain'/>
+          </div>
+        </div>
+      </div>
+      
+      <div className='mt-5'>
+          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+      </div>
+
+      <div className='mt-4 flex flex-wrap gap-2'>
+        {tags.map((tag) => (
+          <p 
+            key={tag.name}
+            className={`text-[14px] ${tag.color}`}
+          >
+            {tag.name}
+          </p>
+        ))}
+      </div>
+      
+    </Tilt>
+  </motion.div>
+ )
 }
 
 
@@ -15,7 +59,7 @@ const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-          <p className={styles.sectionSubText}>My Work</p>
+          <p  className={styles.sectionSubText}>My Work</p>
           <h2 className={styles.sectionHeadText}>Projects</h2>
       </motion.div>
       
@@ -31,7 +75,7 @@ const Works = () => {
         </motion.p>
       </div>
 
-      <div className='mt-20 flex flex-wrap gap-7'>
+      <div className='mt-20 justify-center flex flex-wrap gap-7'>
         {projects.map((project, index) => (
           <ProjectCard 
               key={`project-${index}`}
