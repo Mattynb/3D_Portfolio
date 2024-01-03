3fe568627b04846984d329fe0e07df539959baaa
matheus <matheus.berbet001@umb.edu>
2024-01-03 11:05:18 -0500

    Update Contact and Works components

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -14,7 +14,7 @@ const ProjectCard = ( {index, name, description, tags, image, source_code_link})
   
     <motion.div variants={fadeIn('Up', 'spring', index*0.5, 0.75)}>
       <Tilt
-        className='bg-tertiary p-5 rounded-2x1 sm:w-[360px] w-full' 
+        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full' 
         options={{
           max: 45,
           scale: 1,
@@ -25,7 +25,7 @@ const ProjectCard = ( {index, name, description, tags, image, source_code_link})
           <img 
             src={image}
             alt={name}
-            className='w-full h-full object-cover rounded-2x1'
+            className='w-full h-full object-cover rounded-2xl'
           />
           <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
             <div 
@@ -70,7 +70,7 @@ const Works = () => {
       <div className='w-full flex'>
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
-          className="mt-3 text-secondary text-[17px] max-w-3x1 leading-[30px]"
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
           Following projects showcases my skills and experience through real-world examples of my work. 
           Each project is briefly described with a link to code repositories and live demos in it. 
