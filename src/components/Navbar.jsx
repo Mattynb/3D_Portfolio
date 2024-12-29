2740e8bb9a642f82b39f38b3e5f0634da3be3023
Matheus <matheus.berbet001@umb.edu>
2024-12-28 22:09:41 -0500

    oops 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,12 +11,12 @@ const Navbar = () => {
 
   return (
     <nav
-      className= { `${styles.paddingX} w-full flex justify-between items-center py-5 fixed top-0 z-20 bg-primary`}
+      className={`${styles.paddingX} w-full flex justify-between items-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link 
           to="/" 
-          className = "flex items-center gap-2"
+          className="flex items-center gap-2"
           onClick={() => {
             setActive("");
             window.scrollTo(0, 0);
@@ -26,22 +26,28 @@ const Navbar = () => {
           <p className="text-white-100 text-[23px] font-bold cursor-pointer">Matheus Berbet<span className="sm:block hidden"></span></p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
-          {navLinks.map((Link) => (
+          {navLinks.map((link) => (
             <li
-              key={Link.id}
+              key={link.id}
               className={`
-              ${ active === Link.title 
+              ${active === link.title 
                 ? "text-secondary" 
                 : "text-white-100" 
               }
               hover:text-secondary text-[18px] 
               font-medium cursor-pointer 
               transition duration-300 ease-in-out`}
-              onClick={() => setActive(Link.title)}
+              onClick={() => setActive(link.title)}
             >
-              <a href={`#${Link.id}`}>
-                {Link.title}
-              </a>
+              {link.title === "Contact" ? (
+                <a href="https://www.linkedin.com/in/matheusnberbet/" target="_blank" rel="noopener noreferrer">
+                  {link.title}
+                </a>
+              ) : (
+                <a href={`#${link.id}`}>
+                  {link.title}
+                </a>
+              )}
             </li>
           ))}
         </ul>
@@ -58,31 +64,35 @@ const Navbar = () => {
           p-6 black-gradient absolute top-20 right-0 
           mx-4 my-2 min-2[140px] z-10 rounded-xl`}>
             <ul className="list-none flex justify-end items-start flex-col gap-4">
-              {navLinks.map((Link) => (
+              {navLinks.map((link) => (
                 <li
-                  key={Link.id}
+                  key={link.id}
                   className={`
-                  ${ active === Link.title 
+                  ${active === link.title 
                     ? "text-secondary" 
                     : "text-white-100" 
-                  
                   }
                   font-poppins font-medium 
                   cursor-pointer text-[16px]`}
                   onClick={() => {
-                      setActive(Link.title)
+                      setActive(link.title)
                       setToggle(!toggle)
                     }
                   }
                 >
-                  <a href={`#${Link.id}`}>
-                    {Link.title}
-                  </a>
+                  {link.title === "Contact" ? (
+                    <a href="https://externalwebsite.com" target="_blank" rel="noopener noreferrer">
+                      {link.title}
+                    </a>
+                  ) : (
+                    <a href={`#${link.id}`}>
+                      {link.title}
+                    </a>
+                  )}
                 </li>
               ))}
             </ul>
           </div>
-           
         </div>
       </div>
     </nav>
